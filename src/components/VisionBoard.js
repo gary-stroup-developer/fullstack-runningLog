694996909c288913de6a12e9822e0cb222ea5693
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2021-12-14 06:07:48 -0800

    set up layout of the vision board. set up functionality to send and receive data

--- src/components/VisionBoard.js
+++ src/components/VisionBoard.js
@@ -5,13 +5,14 @@ import { Button, TextInput } from "react-materialize";
 import axios from 'axios';
 
 export const VisionBoard = () => {
+
+    //create an array to hold the images retrieved from db
+    const savedImages = [];
+
     //initialize the state variables
     const [visionTitle, setVisionTitle] = useState('');
-    const [image1, setImage1] = useState('');
-    const [image2, setImage2] = useState('');
-    const [image3, setImage3] = useState('');
-    const [image4, setImage4] = useState('');
-    const [image5, setImage5] = useState('');
+    const [images, setImage] = useState({image1:'',image2: '',image3:'',image4:'',image5:''});
+
     const [message, setMessage] = useState('');
 
     //get the user data
@@ -19,9 +20,11 @@ export const VisionBoard = () => {
     const {id, firstName, userName} = user;
 
     const uploadImages = async () => {
-        const response = await axios.post('/api/upload-images',{userName,id,visionTitle, image1, image2, image3, image4, image5});
+        const response = await axios.post('/api/upload-images',{userName,id,visionTitle, images});
         const message = response.data;
         setMessage(message);
+        setImage({...images,image1:'',image2: '',image3:'',image4:'',image5:''});
+    
         setTimeout(()=> {
             setMessage('');
         },3000);
@@ -31,29 +34,47 @@ export const VisionBoard = () => {
         const getVision = async() => {
         const response = await axios.get('/api/download-mages');
         const {title, image1, image2, image3, image4, image5} = response.data;
+        setVisionTitle(title);
+        savedImages = [...savedImages,image1, image2, image3, image4, image5];
         }
 
         getVision();
     },[]);
 
+    const setImages = (e) => {
+        const [name, value] = e.target;
+        setImage({...images,[name]:value});
+    }
     
     return (
         <div>
             <NavbarComponent name={firstName} username={userName} />
             <h3>Vision Board</h3>
-            {message != ''? message : <p>Get Started with creating your vision of the future!</p>}
             
+            {Object.keys(images).length === 0 ? 
+            <div>
+            <p>Get Started with creating your vision of the future!</p>
+            <p>{message} </p>
             <div className="row">
                 <div clasName="col s-12 m-6 offset-3">
-                <TextInput s={12} id="vision-title" label="Title" placeholder="Title" value={visionTitle} onChange={(e)=>setVisionTitle(e.target.value)} />
-                <TextInput s={12} id="image1" label="First Image" placeholder="Paste the url of the image you want to add" value={image1} onChange={(e)=>setImage1(e.target.value)} />
-                <TextInput s={12} id="image2" label="Second Image" placeholder="Paste the url of the image you want to add" value={image2} onChange={(e)=>setImage2(e.target.value)} />
-                <TextInput s={12} id="image3" label="Third Image" placeholder="Paste the url of the image you want to add" value={image3} onChange={(e)=>setImage3(e.target.value)} />
-                <TextInput s={12} id="image4" label="Fourth Image" placeholder="Paste the url of the image you want to add" value={image4} onChange={(e)=>setImage4(e.target.value)} />
-                <TextInput s={12} id="image5" label="Fifth Image" placeholder="Paste the url of the image you want to add" value={image5} onChange={(e)=>setImage5(e.target.value)} />
-                <Button onClick={uploadImages} className="orange">Upload Images</Button>
+                    <TextInput s={12} id="vision-title" label="Title" placeholder="Title" value={visionTitle} onChange={(e)=>setVisionTitle(e.target.value)} />
+                    <TextInput s={12} id="image1" label="First Image" placeholder="Paste the url of the image you want to add" name="image1" value={images.image1} onChange={(e)=>setImages(e)} />
+                    <TextInput s={12} id="image2" label="Second Image" placeholder="Paste the url of the image you want to add" name="image2" value={images.image2} onChange={(e)=>setImages(e)} />
+                    <TextInput s={12} id="image3" label="Third Image" placeholder="Paste the url of the image you want to add" name="image3" value={images.image3} onChange={(e)=>setImages(e)} />
+                    <TextInput s={12} id="image4" label="Fourth Image" placeholder="Paste the url of the image you want to add" name="image4" value={images.image4} onChange={(e)=>setImages(e)} />
+                    <TextInput s={12} id="image5" label="Fifth Image" placeholder="Paste the url of the image you want to add" name="image5" value={images.image5} onChange={(e)=>setImages(e)} />
+                    <Button onClick={uploadImages} className="orange">Upload Images</Button>
                 </div>
             </div>
+            </div> 
+            : <div className="row">
+            {   savedImages.forEach((img) => {
+                <div className="col" style={{background: `url(${img}) left top no-repeat cover`}} />
+            })
+                
+            }
+            </div>
+            }
         </div>
     )
 }
